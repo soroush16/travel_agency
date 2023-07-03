90007f3c329751aa0b3fa59884f06c2801381f12
soroush16 <ssh.malkp@gmail.com>
2023-07-03 12:38:15 +0300

    tagSlugify slugify() modified

--- src/main/java/com/travelagency/slugify/TagSlugify.java
+++ src/main/java/com/travelagency/slugify/TagSlugify.java
@@ -4,7 +4,7 @@ import java.text.Normalizer;
 import java.util.List;
 
 public class TagSlugify {
-    private final int MAX_SLUG_LENGTH = 256;
+    private static final int MAX_SLUG_LENGTH = 256;
 
     public String slugify(final String s) {
 
@@ -12,7 +12,7 @@ public class TagSlugify {
                 .normalize(s, Normalizer.Form.NFD)
                 .replaceAll("[^\\p{ASCII}]", "")
                 .replaceAll("[^-_a-zA-Z0-9]", "-").replaceAll("\\s+", "-")
-                .replaceAll("[-]+", "-").replaceAll("^-", "")
+                .replaceAll("-+", "-").replaceAll("^-", "")
                 .replaceAll("-$", "").toLowerCase();
         return intermediateResult.substring(0,
                 Math.min(MAX_SLUG_LENGTH, intermediateResult.length()));
