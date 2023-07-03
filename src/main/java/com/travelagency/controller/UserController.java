16642c1724b3f86926775b02b2e06b4a36d60095
soroush16 <ssh.malkp@gmail.com>
2023-07-03 12:46:45 +0300

    UserController fields modified

--- src/main/java/com/travelagency/controller/UserController.java
+++ src/main/java/com/travelagency/controller/UserController.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 @RequestMapping("/api/users")
 public class UserController {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Autowired
     public UserController(UserRepository userRepository) {
