6c64615f55d6122fb33b16f6e59b76d42763c242
soroush16 <ssh.malkp@gmail.com>
2023-07-17 11:35:41 +0300

    User class modified

--- src/main/java/com/travelagency/dto/User.java
+++ src/main/java/com/travelagency/dto/User.java
@@ -4,18 +4,23 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue
     private Long id;
-
     @NotBlank(message = "Name can't be empty")
     private String firstname;
 
@@ -26,6 +31,45 @@ public class User {
     @Email(message = "Email is not correct")
     @Column(unique = true)
     private String email;
+
+    @NotBlank
+    @Column(length = 64)
+    private String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
 
 
