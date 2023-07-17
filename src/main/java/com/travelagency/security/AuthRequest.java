f033ad66aa3c482d7f4c228a09e068a95eed25c7
soroush16 <ssh.malkp@gmail.com>
2023-07-17 11:32:00 +0300

    AuthRequest and AuthResponse classes added

---
+++ src/main/java/com/travelagency/security/AuthRequest.java
@@ -0,0 +1,20 @@
+package com.travelagency.security;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AuthRequest {
+    @NotNull
+    @Email
+    @Length(min = 5, max = 50)
+    private String email;
+
+    @NotNull @Length(min = 5, max = 10)
+    private String password;
+}
---
+++ src/main/java/com/travelagency/security/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.travelagency.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class AuthResponse {
+    private String email;
+    private String accessToken;
+}
