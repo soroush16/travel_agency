f407d167cba2b6969ad1efd615a4c5e08b4e8343
soroush16 <ssh.malkp@gmail.com>
2023-07-17 11:31:32 +0300

    AuthController class added

---
+++ src/main/java/com/travelagency/security/controller/AuthController.java
@@ -0,0 +1,46 @@
+package com.travelagency.security.controller;
+
+import com.travelagency.dto.User;
+import com.travelagency.security.AuthRequest;
+import com.travelagency.security.AuthResponse;
+import com.travelagency.security.JwtTokenUtil;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class AuthController {
+
+    final AuthenticationManager authManager;
+
+    final JwtTokenUtil jwtUtil;
+
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody @Valid AuthRequest request) {
+        try {
+            Authentication authentication = authManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getEmail(), request.getPassword())
+            );
+
+            User user = (User) authentication.getPrincipal();
+            String accessToken = jwtUtil.generateAccessToken(user);
+            AuthResponse response = new AuthResponse(user.getEmail(), accessToken);
+
+            return ResponseEntity.ok().body(response);
+
+        } catch (BadCredentialsException ex) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+}
